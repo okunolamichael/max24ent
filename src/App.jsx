b29df89a07ef366d68e7c504e364cc3a11026e6c
okunolamichael <michaeloolamide@gmail.com>
2025-02-27 18:18:19 -0800

    implementing API_KEY

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>
+            <img src="./MAXENT24_white.png" alt="" className='w-64'/>
+            <img src="hero-img.png" alt="" />
+            Find <span className='text-gradient'>Movie</span>
+            You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
   )
 }
 
