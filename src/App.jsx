771d15f3575f971cf70e858fc6737fd6b89efa99
okunolamichael <michaeloolamide@gmail.com>
2025-03-01 05:30:07 -0800

    search Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Search from './components/Search';
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -15,10 +17,16 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? 
+      `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :
+      `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -26,16 +34,25 @@ const App = () => {
       }
 
       const data = await response.json();
-      console.log(data);
+      
+      if(data.Response === "False") {
+        setErrorMessage(data.Error || 'Error fetching movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchMovies(); // Fixed typo: "fatchMovies" -> "fetchMovies"
-  }, []); // Removed "deps" since it was undefined; empty array means run once on mount
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
@@ -51,9 +68,23 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <section>
-          <h2>All Movies</h2>
-          {errorMessage && <p className="error">{errorMessage}</p>}
+        <section className='all-movies'>
+          <h2 className='mt-[40px]'>All Movies</h2>
+          
+        {isLoading ? (
+          <p className='text-white'><Spinner /></p>
+        ) : errorMessage ? (
+          <p className='text-red-500'>{errorMessage}</p>
+        ) : (
+          <ul>
+            {movieList.map((movie) => (
+              <li>
+                <MovieCard key={movie.id} movie={movie}/>
+                {/* <p className='text-white'>{movie.overview}</p> */}
+              </li>
+            ))}
+          </ul>
+        )}
         </section>
       </div>
     </main>
