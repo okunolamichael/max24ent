b29e5009ac6b786591ef3185d87c21c25025d9b4
okunolamichael <michaeloolamide@gmail.com>
2025-03-02 01:38:45 -0800

    working on appwrite DB

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useDebounce} from 'react-use';
 import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -48,6 +49,11 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
@@ -88,7 +94,6 @@ const App = () => {
             {movieList.map((movie) => (
               <li>
                 <MovieCard key={movie.id} movie={movie}/>
-                {/* <p className='text-white'>{movie.overview}</p> */}
               </li>
             ))}
           </ul>
